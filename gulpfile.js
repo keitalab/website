f4e8286141044cf3a6f68d17e1e3a539146f3a12
mactkg <mactkg@gmail.com>
2015-12-25 01:14:20 +0900

    init nunjuck

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,29 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var nunjucksRender = require('gulp-nunjucks-render');
+nunjucksRender.nunjucks.configure(['src/'], {watch: false});
 
-gulp.task('sass', function () {
+gulp.task('nunjucks', function() {
+  gulp.src(['src/**/*.html', '!src/**/_*.html'])
+    .pipe(nunjucksRender())
+    .pipe(gulp.dest("."));
+});
+
+
+gulp.task('sass', function() {
   gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('nunjucks:watch', function() {
+  gulp.watch('src/**/*.html', ['nunjucks']);
+});
+
+
+gulp.task('sass:watch', function() {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task('watch', ['sass:watch']);
+gulp.task('watch', ['nunjucks:watch', 'sass:watch']);
