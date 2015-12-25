935e2e656be157cf1e2da95fe0e2e48b84527015
mactkg <mactkg@gmail.com>
2015-12-25 16:30:13 +0900

    update workflow challenge

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,52 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var nunjucksRender = require('gulp-nunjucks-render');
-nunjucksRender.nunjucks.configure(['src/'], {watch: false});
+nunjucksRender.nunjucks.configure(['pages/'], {watch: false});
 
 gulp.task('nunjucks', function() {
-  gulp.src(['src/**/*.html', '!src/**/_*.html'])
+  gulp.src(['pages/**/*.html', '!pages/**/_*.html'])
     .pipe(nunjucksRender())
-    .pipe(gulp.dest("."));
+    .pipe(gulp.dest("./dist"));
 });
-
 
 gulp.task('sass', function() {
+  // copy bootstrap files
+  var bs = "./node_modules/bootstrap/dist";
+  gulp.src([bs + '/css/bootstrap-theme.min.css', bs + '/css/bootstrap.min.css'])
+    .pipe(gulp.dest('./dist/css'));
+
+  // compile sass
   gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist/css'));
 });
+
+gulp.task('js', function() {
+  gulp.src('./js/**/*.js')
+    .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('assets', function() {
+  gulp.src('./font/**/*')
+    .pipe(gulp.dest('./dist/font'));
+
+  gulp.src('./image/**/*')
+    .pipe(gulp.dest('./dist/image'));
+});
+
 
 gulp.task('nunjucks:watch', function() {
-  gulp.watch('src/**/*.html', ['nunjucks']);
+  gulp.watch('pages/**/*.html', ['nunjucks']);
 });
 
+gulp.task('js:watch', function() {
+  gulp.watch('./js/**/*.js', ['js']);
+});
 
 gulp.task('sass:watch', function() {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task('watch', ['nunjucks:watch', 'sass:watch']);
+
+gulp.task('build', ['nunjucks', 'js', 'sass', 'assets']);
+gulp.task('watch', ['nunjucks:watch', 'js:watch', 'sass:watch']);
