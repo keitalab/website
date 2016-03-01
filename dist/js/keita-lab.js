aa13fb7b2adc3b699e55b9a80fff44068f8bcc0f
62grow2e <amglowez@gmail.com>
2016-03-02 00:38:15 +0900

    Added slideshow

--- dist/js/keita-lab.js
+++ dist/js/keita-lab.js
@@ -1,4 +1,39 @@
+// Definitions
+var slideshow = {
+  fadeDuration: 800,
+  interval: 3000
+}
+// Slideshow functions
+var initSlideshow = function() {
+  $.each($('section.slideshow div.slideshow-item'), function(i) {
+    $('section.slideshow div.slideshow-item')
+      .eq(i)
+      .css('display', 'none');
+  });
+  $('section.slideshow div.slideshow-item')
+    .eq(0)
+    .fadeIn(slideshow.fadeDuration);
+}
+var updateSlideshow = function() {
+  setInterval(function() {
+    var active = $('section.slideshow div.active');
+    var next = active.next().length?
+      active.next(): $('section.slideshow div.slideshow-item').eq(0);
+    active.fadeOut(slideshow.fadeDuration, function() {
+      active.removeClass('active');
+    });
+    next.fadeIn(slideshow.fadeDuration, function() {
+      next.addClass('active');
+    });
+  }, slideshow.interval);
+}
+
+// Loading
+initSlideshow();
+// Loaded
 $(window).load(function(){
+  $('section.slideshow div.active')
+    .fadeIn(slideshow.fadeDuration, updateSlideshow());
 /*
   var $window = $(window);
   //smooth scroll
